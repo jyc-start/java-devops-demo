28f58fd30ac89bd88b92b2a06e7bbb36fa79c092
jyc <15340685994@189.cn>
2022-08-11 17:02:26 +0800

    init

---
+++ src/test/java/com/dachang/javadevopsdemo/JavaDevopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dachang.javadevopsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevopsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
